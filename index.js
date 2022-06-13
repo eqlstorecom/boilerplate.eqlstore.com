eb9f929accc5ae9fd5011e804ae0031eb479ac87
young sik seo <angelskying@gmail.com>
2022-06-13 18:01:46 +0900

    fafstify port 버그 수정진행함

--- index.js
+++ index.js
@@ -87,12 +87,12 @@ import rate_limit from '@fastify/rate-limit';
 			(req, res) => handler(req.raw, res.raw, () => {}),
 		);
 
-		app.listen({ port: 3000 }, '0.0.0.0', (err, address) => {
+		app.listen({ port: 18082 }, '0.0.0.0', (err, address) => {
 			if (err) {
-				fastify.log.error(err);
+				app.log.error(err);
 				process.exit(1);
 			}
-			fastify.log.info(`server listening on ${address}`);
+			app.log.info(`server listening on ${address}`);
 		});
 
 		// await app.listen(
