1519b5890a4d647d03e64d7a28f5eac4c6ce4cd4
young sik seo <angelskying@gmail.com>
2022-06-13 17:41:29 +0900

    fastify,  kitql Build 서버 세팅 진행

--- index.js
+++ index.js
@@ -1,28 +1,95 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true });
+import { handler } from './build/handler.js';
+import fastify from 'fastify';
+import path from 'path';
+import fastifyStatic from '@fastify/static';
+import helmet from '@fastify/helmet';
+import rate_limit from '@fastify/rate-limit';
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-	return { hello: 'world' };
-});
-
-// Run the server!
-const start = async () => {
+(async () => {
 	try {
-		await fastify.listen(
-			process.env.PORT || 18082,
-			'0.0.0.0',
-			function (err, address) {
-				if (err) {
-					fastify.log.error(err);
-					process.exit(1);
-				}
-				fastify.log.info(`server listening on ${address}`);
+		//fastify
+		const app = fastify({
+			connectionTimeout: 0,
+			keepAliveTimeout: 5000,
+			maxRequestsPerSocket: 0,
+			requestTimeout: 0,
+			ignoreTrailingSlash: true,
+			bodyLimit: 1024 * 1024 * 10,
+			disableRequestLogging: false,
+		});
+
+		//necessary!!!
+		app.removeAllContentTypeParsers();
+		app.addContentTypeParser('*', (req, payload, done) => done(null, null));
+
+		//rate limit
+		await app.register(rate_limit, {
+			cache: 10000,
+			allowList: ['127.0.0.1', 'localhost'],
+			keyGenerator: req =>
+				req.headers['x-real-ip'] ||
+				req.headers['x-client-ip'] ||
+				req.headers['x-forwarded-for'] ||
+				req.ip,
+		});
+
+		await app.register(helmet, {
+			contentSecurityPolicy: false,
+			crossOriginEmbedderPolicy: false,
+		});
+
+		//other path
+		app.get('/check', async (req, res) => {
+			return 'ok';
+		});
+
+		//rate limit
+		app.get(
+			'/429',
+			{
+				config: {
+					rateLimit: {
+						max: async (req, key) => {
+							return 1000;
+						},
+						timeWindow: 1000 * 60,
+					},
+				},
 			},
+			(req, res) => handler(req.raw, res.raw, () => {}),
 		);
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
+
+
+		app.all(
+			'/*',
+			{
+				config: {
+					rateLimit: {
+						max: async (req, key) => {
+							console.log('req.url', req.url.startsWith('/api/'));
+							if (req.url.startsWith('/api/')) {
+								return 500;
+							} else {
+								return 1000;
+							}
+						},
+						timeWindow: 1000 * 60,
+					},
+				},
+				errorHandler: (error, req, res) => {
+					if (error.message.startsWith('Rate limit exceeded')) {
+						res.code(302).redirect('/429');
+					} else {
+						throw error;
+					}
+				},
+			},
+			(req, res) => handler(req.raw, res.raw, () => {}),
+		);
+
+		await app.listen(process.env.PORT || 18082);
+	} catch (e) {
+		console.log(e);
+		process.exit(-1);
 	}
-};
-start();
+})();
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,35 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+import watchAndRun from '@kitql/vite-plugin-watch-and-run';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
-	kit: {
-		adapter: adapter(),
+	experimental: {
+		useVitePreprocess: true
 	},
+	kit: {
+		// adapter: adapter()
+		adapter: adapter({
+			out: 'build',
+			precompress: true,
+			envPrefix: ''
+		}),
+		vite: {
+			plugins: [
+				watchAndRun([
+					{
+						watch: '**/*.(gql|graphql)',
+						run: 'npm run gen'
+					}
+				])
+			]
+		}
+	}
 };
 
 export default config;
