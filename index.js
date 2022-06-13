bc1648d77769b96fadc455275750a987b113ff1a
young sik seo <angelskying@gmail.com>
2022-06-13 17:59:16 +0900

    fastify 버그 수정진행함

--- index.js
+++ index.js
@@ -87,17 +87,25 @@ import rate_limit from '@fastify/rate-limit';
 			(req, res) => handler(req.raw, res.raw, () => {}),
 		);
 
-		await app.listen(
-			process.env.PORT || 18082,
-			'0.0.0.0',
-			function (err, address) {
-				if (err) {
-					fastify.log.error(err);
-					process.exit(1);
-				}
-				fastify.log.info(`server listening on ${address}`);
-			},
-		);
+		app.listen({ port: 3000 }, '0.0.0.0', (err, address) => {
+			if (err) {
+				fastify.log.error(err);
+				process.exit(1);
+			}
+			fastify.log.info(`server listening on ${address}`);
+		});
+
+		// await app.listen(
+		// 	process.env.PORT || 18082,
+		// 	'0.0.0.0',
+		// 	(err, address) =>{
+		// 		if (err) {
+		// 			fastify.log.error(err);
+		// 			process.exit(1);
+		// 		}
+		// 		fastify.log.info(`server listening on ${address}`);
+		// 	},
+		// );
 	} catch (e) {
 		console.log(e);
 		process.exit(-1);
