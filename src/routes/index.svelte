3411ef3aa6fe02dc607cd2f05d30bc1c4674adda
yuri <noobish.in@gmail.com>
2022-06-14 10:25:52 +0900

    SMUI 라이브러리 추가

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import Button, { Label } from '@smui/button';
+
+	let clicked = 0;
 </script>
 
 <h1>Welcome to SvelteKit!!!!!!!</h1>
 <p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+	documentation
+
+	<!-- materualui 테스트 -->
+	<Button on:click={() => clicked++}>
+		<Label>Default</Label>
+	</Button>
 </p>
+
+<pre class="status">Clicked: {clicked}</pre>
