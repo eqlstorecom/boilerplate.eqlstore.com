b701f130d2a45a7538b902657191bcf62da01f77
young sik seo <angelskying@gmail.com>
2022-06-14 11:46:54 +0900

    서버 로그 수정진행함

--- index.js
+++ index.js
@@ -86,13 +86,14 @@ import rate_limit from '@fastify/rate-limit';
 			(req, res) => handler(req.raw, res.raw, () => {}),
 		);
 
-		await app.listen({ port: 18082 }, '0.0.0.0', (err, address) => {
+		app.listen({ port: 18082 }, '0.0.0.0', (err, address) => {
 			if (err) {
 				app.log.error(err);
 				process.exit(1);
 			}
-			// app.log.info(`server listening on ${address}`);
+			console.log(`server listening on ${address}`);
 		});
+
 	} catch (e) {
 		console.log(e);
 		process.exit(-1);
