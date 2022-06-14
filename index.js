c231c2803cd7864446270514836330d3beeba1f9
young sik seo <angelskying@gmail.com>
2022-06-14 11:22:33 +0900

    await 추가진행함

--- index.js
+++ index.js
@@ -86,25 +86,13 @@ import rate_limit from '@fastify/rate-limit';
 			(req, res) => handler(req.raw, res.raw, () => {}),
 		);
 
-		app.listen({ port: 18082 }, '0.0.0.0', (err, address) => {
+		await app.listen({ port: 18082 }, '0.0.0.0', (err, address) => {
 			if (err) {
 				app.log.error(err);
 				process.exit(1);
 			}
 			// app.log.info(`server listening on ${address}`);
 		});
-
-		// await app.listen(
-		// 	process.env.PORT || 18082,
-		// 	'0.0.0.0',
-		// 	(err, address) =>{
-		// 		if (err) {
-		// 			fastify.log.error(err);
-		// 			process.exit(1);
-		// 		}
-		// 		fastify.log.info(`server listening on ${address}`);
-		// 	},
-		// );
 	} catch (e) {
 		console.log(e);
 		process.exit(-1);
